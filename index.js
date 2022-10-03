6cfb11e688eb818723a83c56550c7f86c99e2f6e
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-10-03 10:59:34 -0400

    final edit

--- index.js
+++ index.js
@@ -32,7 +32,19 @@ const search = (event) => {
       const drinkRecipeInstructions = document.createElement('h2')
       drinkRecipeInstructions.innerHTML = drink.strInstructions
       drinkDiv.append(drinkRecipeInstructions)
-      
+
+      const alcoholic = document.createElement('h3')
+      alcoholic.innerHTML = drink.strAlcoholic
+      drinkDiv.append(alcoholic)
+      alcoholic.addEventListener('mouseover', () => {
+        alcoholic.style.cssText = "color: purple"
+      })
+
+    
+      alcoholic.addEventListener('mouseout', () => {
+        alcoholic.style.cssText = "color: black"
+      })
+
       let count = 0;
       const likeCount = document.createElement("p");
       likeCount.className = "likes-count";
@@ -46,8 +58,10 @@ const search = (event) => {
       like.addEventListener('click', () => {
         count ++
         likeCount.textContent = `${count} likes`;
+
+
+        
       })
-    
       searchResults.parentNode.insertBefore(drinkDiv, searchResults.nextSibling)
     })
     )
@@ -55,7 +69,6 @@ const search = (event) => {
 };
 document.addEventListener('submit', search);
 
-      
       function nightMode() {
       const slider = document.getElementById("nightMode");
         const body = document.querySelector("body");
@@ -70,74 +83,6 @@ document.addEventListener('submit', search);
           body.style.cssText = 'background: lightblue;'
         }
       }
-
-      
-//OPTION4 MOUSEOVER
-
-      const cardHover = document.querySelectorAll(".card");
-      cardHover.addEventListener('mouseover', myScript) 
-      function myScript() {
-        cardHover.style.cssText = 'border: purple'
-      }
-
-      // const searchResults = document.getElementById('searchResults');
-      // searchResults.addEventListener("mouseover", myScript);
-
-      //OPTION3 MOUSEOVER
-      // const searchResults = document.getElementById ('searchResults');
-      
-      // searchResults.addEventListener("mouseenter", (e) => {
-      //   searchResults.style.border = '5px dotted orange';
-      // })
-
-      // searchResults.addEventListener("mouseleave", (e) => {
-      //   searchResults.style.border = '1px solid yellow';
-      // })
-
-      //mouseover option
-// var item = document.getElementById("searchResults");
-// item.addEventListener("mouseover", func, false);
-
-// function func() {
-//   var item = document.getElementById("searchResults");
-//   item.setAttribute("style", "background-color:blue;")
-// }
-
-//OPTION1 MOUSEOVER searchResults=target
-      // const searchResults = document.querySelector('.img');
-      // searchResults.addEventListener('mouseover', mOver, false);
-      // searchResults.addEventListener('mouseout', mOut, false);
-
-      // function mOver() {
-      //   searchResults.setAttribute("style", "background-color:purple;")
-      // }
-
-      // function mOut() {
-      //   searchResults.setAttribute("style", "background-color:pink;")
-      // }
-
-
-//OPTION2 MOUSEOVER
-      // const hover = false;
-      // document.getElementById("searchResults").addEventListener("mouseover", function(event) {
-      // // console.log('called on hover'); //just to make sure the function is getting called   
-      // if(!hover) {
-      //     buttonTL.reversed() ? buttonTL.restart() : buttonTL.reverse();
-      // }
-      //     hover = true;
-      // }, false);
-
-      // document.getElementById("searchResults").addEventListener("mouseleave", function( event ) {
-      // // console.log('called on dhjhhhjs'); //just to make sure the function is getting called   
-      // hover = false;
-      // buttonTL.reversed() ? buttonTL.restart() : buttonTL.reverse();
-      // }, false);
-
-
-    
-
-
-
 
 
 //Show image at page open at all times
